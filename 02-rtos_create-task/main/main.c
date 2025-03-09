66f17caf19e15dcf35f2cacd0b6b2f8ada8b837c
ZQCsky <18392797975@163.com>
2025-03-09 14:04:10 +0800

    同步创建任务例程

--- 02-rtos_create-task/main/main.c
+++ 02-rtos_create-task/main/main.c
@@ -5,11 +5,25 @@
 
 static const char* TAG = "main";
 
-void app_main(void)
+void task_1(void* pvParameter)
 {
     while(1)
     {
-        ESP_LOGI(TAG,"Hello world On 02-rtos_create-task!");
+        ESP_LOGI(TAG, "task 1");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+}
+
+void task_2(void* pvParameter)
+{
+    while(1)
+    {
+        ESP_LOGI(TAG, "task 2");
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+void app_main(void)
+{
+    xTaskCreatePinnedToCore(task_1, "task 1", 2048, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(task_2, "task 2", 2048, NULL, 1, NULL, 1);
 }
