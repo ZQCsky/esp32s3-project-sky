3ce8a825251db567d64093497f78e5a0ae95d799
ZQCsky <18392797975@163.com>
2025-03-11 18:44:16 +0800

    更新WS2812例程

--- 07-driver_ws2812b/main/main.c
+++ 07-driver_ws2812b/main/main.c
@@ -1,98 +1,98 @@
-#include <stdio.h>
+/*
+ * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-#include "driver/ledc.h"
-#include "driver/gpio.h"
-#include "freertos/event_groups.h"
-TaskHandle_t task1_handle = NULL;
+#include "driver/rmt_tx.h"
+#include "led_ws2812.h"
 
-/**
- * 源地的板子没有LED，使用GPIO1
- */
-#define LED_GPIO_PIN GPIO_NUM_1
-#define LEDC_EV_FINISH_FULL         (1<<0)
-#define LEDC_EV_FINISH_EMPTY        (1<<1)
+#define WS2812_GPIO_NUM     GPIO_NUM_48
+#define WS2812_LED_NUM      1
 
-bool IRAM_ATTR ledc_finish_cb(const ledc_cb_param_t *param, void *user_arg)
+/** HSV转RGB ，暂无用到
+ * @param h:色调(0-360) s饱和度(0-100) v亮度(0-100)
+ * @param rgb
+ * @return 无
+*/
+void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t *g, uint32_t *b)
 {
-    BaseType_t pxHigherPriorityTaskWoken= 0;
-    if(param->duty == 0)
-        xTaskNotifyFromISR(task1_handle,param->duty,eSetValueWithOverwrite,&pxHigherPriorityTaskWoken);
-    else
-        xTaskNotifyFromISR(task1_handle,param->duty,eSetValueWithOverwrite,&pxHigherPriorityTaskWoken);
-    return pxHigherPriorityTaskWoken;
-}
-ledc_cbs_t cbs = {
-    .fade_cb = ledc_finish_cb, // 渐变完成回调函数
-};
-void task_1(void* pvParameter)
-{
-    //bool led_state = 0;
-    uint32_t notify_value = 0;
-    while(1)
-    {
-        // gpio_set_level(LED_GPIO_PIN, led_state);
-        // led_state = !led_state;
-        // vTaskDelay(pdMS_TO_TICKS(500));
-        xTaskNotifyWait(0, ULONG_MAX, &notify_value,portMAX_DELAY);
-        if(notify_value != 0)
-        {
-            ledc_set_fade_with_time(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, 0, 2000); // 2000ms
-            ledc_fade_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, LEDC_FADE_NO_WAIT);
-            ESP_LOGI("Task1", "task_1 get notify value %ld", notify_value);
-            notify_value = 0;
-        }
-        else
-        {
-            ledc_set_fade_with_time(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, 8191, 2000); // 2000ms
-            ledc_fade_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, LEDC_FADE_NO_WAIT);
-            ESP_LOGI("Task1", "task_1 get notify value %ld", notify_value);
-            notify_value = 0;
-        }
-        ledc_cb_register(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, &cbs, NULL);
+    h %= 360; // h -> [0,360]
+    uint32_t rgb_max = v * 2.55f;
+    uint32_t rgb_min = rgb_max * (100 - s) / 100.0f;
+
+    uint32_t i = h / 60;
+    uint32_t diff = h % 60;
+
+    // RGB adjustment amount by hue
+    uint32_t rgb_adj = (rgb_max - rgb_min) * diff / 60;
+
+    switch (i) {
+    case 0:
+        *r = rgb_max;
+        *g = rgb_min + rgb_adj;
+        *b = rgb_min;
+        break;
+    case 1:
+        *r = rgb_max - rgb_adj;
+        *g = rgb_max;
+        *b = rgb_min;
+        break;
+    case 2:
+        *r = rgb_min;
+        *g = rgb_max;
+        *b = rgb_min + rgb_adj;
+        break;
+    case 3:
+        *r = rgb_min;
+        *g = rgb_max - rgb_adj;
+        *b = rgb_max;
+        break;
+    case 4:
+        *r = rgb_min + rgb_adj;
+        *g = rgb_min;
+        *b = rgb_max;
+        break;
+    default:
+        *r = rgb_max;
+        *g = rgb_min;
+        *b = rgb_max - rgb_adj;
+        break;
     }
 }
 
-static void ledc_init(void)
-{
-    // 初始化GPIO
-    gpio_config_t io_conf = {
-        .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = 1ULL << LED_GPIO_PIN,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE,
-    };
-    gpio_config(&io_conf);
-    ledc_timer_config_t ledc_timer = {
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .duty_resolution = LEDC_TIMER_13_BIT,
-        .timer_num = LEDC_TIMER_0,      //定时器通道0 
-        .freq_hz = 5000,
-        .clk_cfg = LEDC_AUTO_CLK, 
-    };  
-    ledc_timer_config(&ledc_timer);
-    ledc_channel_config_t ledc_channel = {
-       .gpio_num = LED_GPIO_PIN,
-       .speed_mode = LEDC_LOW_SPEED_MODE,
-       .channel = LEDC_CHANNEL_0,
-       .intr_type = LEDC_INTR_DISABLE,
-       .timer_sel = LEDC_TIMER_0,
-       .duty = 0,
-    };
-    ledc_channel_config(&ledc_channel);
-
-    ledc_fade_func_install(0); // 开启PWM模块
-    ledc_set_fade_with_time(ledc_channel.speed_mode, ledc_channel.channel, 8191, 2000); // 2000ms
-    ledc_fade_start(ledc_channel.speed_mode, ledc_channel.channel, LEDC_FADE_NO_WAIT);
-
-    ledc_cb_register(ledc_channel.speed_mode, ledc_channel.channel, &cbs, NULL);
-}
 void app_main(void)
 {
-    // 外设初始化
-    ledc_init();
-    // 创建任务
-    xTaskCreatePinnedToCore(task_1, "task 1", 4096, NULL, 1, &task1_handle, 1);
+    ws2812_strip_handle_t ws2812_handle = NULL;
+    int index = 0;
+    ws2812_init(WS2812_GPIO_NUM,WS2812_LED_NUM,&ws2812_handle);
+
+    while(1)
+    {
+        //红色跑马灯
+        for(index = 0;index < WS2812_LED_NUM;index++)
+        {
+            uint32_t r = 100,g = 5,b = 5;
+            ws2812_write(ws2812_handle,index,r,g,b);
+            vTaskDelay(pdMS_TO_TICKS(180));
+        }
+        //绿色跑马灯
+        for(index = 0;index < WS2812_LED_NUM;index++)
+        {
+            uint32_t r = 5,g = 100,b = 5;
+            ws2812_write(ws2812_handle,index,r,g,b);
+            vTaskDelay(pdMS_TO_TICKS(180));
+        }
+        //蓝色跑马灯
+        for(index = 0;index < WS2812_LED_NUM;index++)
+        {
+            uint32_t r = 5,g = 5,b = 100;
+            ws2812_write(ws2812_handle,index,r,g,b);
+            vTaskDelay(pdMS_TO_TICKS(180));
+        }
+    }
+
 }
